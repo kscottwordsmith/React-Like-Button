36a58ac92283071b06958ebe0a9743411239128c
kscottwordsmith <kscottwordsmith@gmail.com>
2018-12-26 14:11:26 -0800

    button complete

--- src/App.js
+++ src/App.js
@@ -1,27 +1,40 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+
+
 class App extends Component {
+  state = {
+    likes: 0,
+    word: "likes",
+  }
+
+  handleClick = (e) => {
+    let temp = this.state.likes + 1
+    this.setState({
+      likes: temp
+    })
+    this.grammar(temp)
+  }
+
+  grammar = (num) => {
+    if (num === 1) {
+      this.setState({
+        word: "like"
+      })
+    } else {
+      this.setState({
+        word: "likes"
+      })
+    }
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <button id="likes" onClick={this.handleClick}>{this.state.likes} {this.state.word}</button>
       </div>
-    );
+    )
   }
 }
 
